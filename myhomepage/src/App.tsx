2eee45f585d5277d95ad7da01a445bf16ff74fe8
“ksj686” <kim_se_joon@naver.com>
2025-06-02 13:26:07 +0900

    0602

--- myhomepage/src/App.tsx
+++ myhomepage/src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 // import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import Layout from './components/Layout';
-import About from './pages/About';
-import Skill from './pages/Skill';
-import Portfolio from './pages/Portfolio';
-import Todolist from './pages/Todolist';
-import Contact from './pages/Contact';
-import "./App.scss"
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Skill from "./pages/Skill";
+import Portfolio from "./pages/Portfolio";
+import Todolist from "./pages/Todolist";
+import Contact from "./pages/Contact";
+import "./App.scss";
 
 function App() {
   return (
--- myhomepage/src/pages/Contact.tsx
+++ myhomepage/src/pages/Contact.tsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import SectionTitle from '../components/SectionTitle'
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import SectionTitle from "../components/SectionTitle";
+
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-    return (
-        <div className="main contact">
-            <div className='content-inner'>
-                <SectionTitle title="Contact Us" />
-                <div>contact 본문</div>
-            </div>
-        </div>
-    )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs.send(
+      "service_cnymxdd", //service_id
+      "" //
+    );
+  };
+
+  return (
+    <div className="main contact">
+      <div className="content-inner">
+        <SectionTitle title="Contact Us" />
+        <div className="contact-wrap">
+          <div>
+            <form onSubmit={handleSubmit}>
+              <p>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="이름"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </p>
+              <p>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="이메일"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </p>
+              <p>
+                <textarea
+                  name="message"
+                  id=""
+                  placeholder="메세지를 입력하세요."
+                  value={formData.message}
+                  onChange={handleChange}
+                ></textarea>
+              </p>
+              <button type="submit">전송</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
